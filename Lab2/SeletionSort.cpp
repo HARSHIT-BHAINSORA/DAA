e1d92590c44e41dc0c68c23d1cdcc7b18d81036f
Harshit Bhainsora <90299672+HARSHIT-BHAINSORA@users.noreply.github.com>
2022-03-24 10:16:16 +0530

    completed

---
+++ Lab2/SeletionSort.cpp
@@ -0,0 +1,16 @@
+// internal sorting {need whole memory of their array}
+// 1. bubble ,selection , insertion
+
+// external {take half array in memory} merge sort.
+
+/* inplace  = no extra space taken O(1) <= x <= logn
+ outplace = extra space taken 
+
+ stable  = order should be maintaine.
+ unstable = opposite of stable.
+
+// -2 45 0 11 -9 
+
+#include<iostream>
+using namespace std;
+
